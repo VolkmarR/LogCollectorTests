fec8b792a8c6c74de5bdec1b0f14a6235a88cee9
Volkmar Rigo <volkmar@rigo.bz>
2022-03-20 10:48:12 +0100

    Created Asp.net Core Solution

---
+++ src/LogCollectorTests/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LogCollectorTests.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogDebug("Simple debug from OnGet");
+            _logger.LogInformation("Simple information from OnGet");
+            _logger.LogWarning("Simple warning from OnGet");
+            _logger.LogError("Simple error from OnGet");
+            _logger.LogCritical("Simple critical from OnGet");
+        }
+    }
+}
